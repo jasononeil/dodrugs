7d9c11027faadb5ec98bd16722ce70b5b18636d8
Jason O'Neil <jason.oneil@gmail.com>
2017-11-26 19:55:49 +1100

    Add an assertion so that the tests no longer generate a warning

--- test/issues/TestIssue17.hx
+++ test/issues/TestIssue17.hx
@@ -14,7 +14,8 @@ class TestIssue17 {
 		// Previously this generated the error: `Any should be tink.core.Any`
 		// The fact that this compiles is sufficient.
 		var inj = Injector.create("TestIssue17", [
-			var _:String = '',
+			var _:String = 'hello',
 		]);
+		Assert.equals("hello", inj.get(String));
 	}
 }
